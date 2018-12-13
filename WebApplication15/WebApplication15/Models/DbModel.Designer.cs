682d8fd6f642c2b0f6e08f067d23e3318a93bbfb
mamona-zahoor <uetian997@gmail.com>
2018-12-12 19:30:03 -0800

    Logged in view and modifications in Account views.

--- WebApplication15/WebApplication15/Models/DbModel.Designer.cs
+++ WebApplication15/WebApplication15/Models/DbModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Admin\Desktop\YaAllah Reham plz\LMSProject\WebApplication15\WebApplication15\Models\DbModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Mamuna\Desktop\ABC\LMSProject\WebApplication15\WebApplication15\Models\DbModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
